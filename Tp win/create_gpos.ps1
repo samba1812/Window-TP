7294780ae122a512057cab3195ee166f071d7c64
samba1812 <145571342+samba1812@users.noreply.github.com>
2025-02-06 19:00:47 +0100

    gpo

--- Tp win/create_gpos.ps1
+++ Tp win/create_gpos.ps1
@@ -1,17 +1,34 @@
 # Import du module GroupPolicy
 Import-Module GroupPolicy
 
-# Définition des variables
+# Récupération du nom de domaine
 $DomainName = (Get-ADDomain).DistinguishedName
-$ExamOU = "OU=EXAM,$DomainName"
 
-# Chemin du fond d'écran
+# Chemins des fonds d'écran (à adapter selon votre environnement)
 $CorpWallpaper = "\\DC01\Partages\Commun\wallpapers\UL.jpg"
+$HRWallpaper = "\\DC01\Partages\Commun\wallpapers\HR.jpg"
+$ITWallpaper = "\\DC01\Partages\Commun\wallpapers\IT.jpg"
 
-# Création de la GPO CORPWP
+# Création et configuration de la GPO Corporate
 $CORPWP = New-GPO -Name "CORPWP"
-New-GPLink -Name "CORPWP" -Target $ExamOU
+New-GPLink -Name "CORPWP" -Target $DomainName
 Set-GPRegistryValue -Name "CORPWP" -Key "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System" `
     -ValueName "Wallpaper" -Type String -Value $CorpWallpaper
 Set-GPRegistryValue -Name "CORPWP" -Key "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System" `
+    -ValueName "WallpaperStyle" -Type String -Value "2"
+
+# Création et configuration de la GPO HR
+$HRWP = New-GPO -Name "HRWP"
+New-GPLink -Name "HRWP" -Target "OU=HR,$DomainName"
+Set-GPRegistryValue -Name "HRWP" -Key "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System" `
+    -ValueName "Wallpaper" -Type String -Value $HRWallpaper
+Set-GPRegistryValue -Name "HRWP" -Key "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System" `
+    -ValueName "WallpaperStyle" -Type String -Value "2"
+
+# Création et configuration de la GPO IT
+$ITWP = New-GPO -Name "ITWP"
+New-GPLink -Name "ITWP" -Target "OU=IT,$DomainName"
+Set-GPRegistryValue -Name "ITWP" -Key "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System" `
+    -ValueName "Wallpaper" -Type String -Value $ITWallpaper
+Set-GPRegistryValue -Name "ITWP" -Key "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System" `
     -ValueName "WallpaperStyle" -Type String -Value "2"
